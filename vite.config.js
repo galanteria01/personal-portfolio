531e7d58a2e2071addb2beb38cefba1ba3cb9834
Shoury Sharma <shanuu12e@gmail.com>
2023-02-05 11:07:50 +0530

    feat: add deployment script and config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: './src',
+  base: "./",
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        projects: resolve(__dirname, 'src/projects.html'),
+        contact: resolve(__dirname, 'src/contact.html'),
+        main: resolve(__dirname, 'src/index.html'),
+      }
+    }
   }
 })
